ec15502c36b89d4895d7ea5be07d50deeeccc88d
Cristian Nazareno Gonzalez <43855102+crisgonzalez94@users.noreply.github.com>
2021-02-26 15:59:40 -0300

    first commit

---
+++ Assets/Components.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Components : MonoBehaviour
+{
+    //Creando el tipo de dato
+    public enum Brujula {none , Norte , Sur , Este , Oeste}
+
+    //Creando una variable de tipo brujula
+    public Brujula miDireccion = Brujula.Norte;
+
+    
+
+    void Start(){
+
+
+    }
+    
+    void Update(){
+        //Invocamos a la funcion
+        ImprimirDireccion(miDireccion);
+
+    }
+
+    //Tambien podremos añadir funciones a los enum.
+    Brujula ImprimirDireccion(Brujula dir){
+        Debug.Log(dir);
+        return dir;
+    }
+
+}
